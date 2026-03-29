565a8103e7e28a26289673cfd13c0bf2dd6053ca
Gabriel Ing <ging@intersystems.com>
2026-03-29 12:51:20 +0100

    usable state

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -7,6 +7,7 @@ contextBridge.exposeInMainWorld('presentApi', {
   deckImport: () => ipcRenderer.invoke('deck:import'),
   deckExport: (deckId) => ipcRenderer.invoke('deck:export', deckId),
   pickSlideFiles: () => ipcRenderer.invoke('deck:pickSlideFiles'),
+  pickSlideDirectory: () => ipcRenderer.invoke('deck:pickSlideDirectory'),
   importSlides: (params) => ipcRenderer.invoke('deck:importSlides', params),
   resolveSlideUrl: (params) => ipcRenderer.invoke('deck:resolveSlideUrl', params),
   resolveSlideDataUrl: (params) => ipcRenderer.invoke('deck:resolveSlideDataUrl', params),
